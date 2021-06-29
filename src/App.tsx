7a5cb1f87c1651d9dd403a3fe26eadd5ad7aa159
shahmansi2197 <mansi.mobilefirst@gmail.com>
2021-06-29 17:30:42 +0530

    Jss Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import List from './Component/List';
 import AddToList from './Component/AddToList';
+import  { styleCss } from './StyleCss';
+
 
 
 export interface IState {
@@ -17,12 +19,13 @@ export interface IState {
 function App() {
 
   const [people, setPeople] = useState<IState["people"]>([])
+  const styleClass =  styleCss();
 
   return (
     <div className="App">
      
       <AddToList setPeople={setPeople} people={people}/>
-      <h1>People Invited to my Party</h1>
+      <h1 className={styleClass.Listheader} >People Invited to my Party</h1>
       <List people={people}/>
     </div>
   );
